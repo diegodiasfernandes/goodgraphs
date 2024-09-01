09f4f5d9743b77fd23089f318bc25b25b151c803
Diego Dias <diegodiasfernandes.comp@gmail.com>
2024-09-01 14:52:02 -0300

    removing comments

--- SearchClasses/DFS.py
+++ SearchClasses/DFS.py
@@ -28,11 +28,10 @@ class DFS:
         self.last_initial = initial
 
     def search(self, vertex: int):
-        # Pilha para armazenar o estado da busca
         stack = [(vertex, iter(self.graph.neighbors(vertex)))]
 
         while stack:
-            current_vertex, neighbors = stack[-1]  # Verifica o topo da pilha
+            current_vertex, neighbors = stack[-1]
             if self.color[current_vertex] == 'white':
                 self.time += 1
                 self.t_init[current_vertex] = self.time
@@ -44,11 +43,10 @@ class DFS:
                     self.pi[neighbor] = current_vertex
                     stack.append((neighbor, iter(self.graph.neighbors(neighbor))))
             except StopIteration:
-                # Todos os vizinhos foram visitados, marcar como 'black'
                 self.color[current_vertex] = 'black'
                 self.time += 1
                 self.t_finish[current_vertex] = self.time
-                stack.pop()  # Remove o vértice da pilha após finalizar
+                stack.pop()
 
     def showResults(self):
         print("--------------------------------------------")
